a98da2a72ab7dd804feb0db4dbd6cf0aeaade8c8
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-07-09 23:19:53 -0400

    Enable and disable test

--- src/main/java/com/github/mayankpatibandla/testplugin/TestPlugin.java
+++ src/main/java/com/github/mayankpatibandla/testplugin/TestPlugin.java
@@ -7,11 +7,13 @@ public final class TestPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Test Plugin has started!");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Test Plugin has stopped!");
     }
 }
