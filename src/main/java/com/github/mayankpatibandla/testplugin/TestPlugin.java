101d7d8b037618e4136f6484f300876f8896f5d0
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-07-09 23:20:05 -0400

    Player join event

--- src/main/java/com/github/mayankpatibandla/testplugin/TestPlugin.java
+++ src/main/java/com/github/mayankpatibandla/testplugin/TestPlugin.java
@@ -1,14 +1,23 @@
 package com.github.mayankpatibandla.testplugin;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TestPlugin extends JavaPlugin {
+public final class TestPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Test Plugin has started!");
 
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(event.getPlayer().getName() + " joined the server!");
     }
 
     @Override
