dbb04075e5ecede97fadaafcc3ae573aa5ca527a
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-07-10 23:05:48 -0400

    Feed command improvements

--- src/main/java/com/github/mayankpatibandla/testplugin/commands/FeedCommand.java
+++ src/main/java/com/github/mayankpatibandla/testplugin/commands/FeedCommand.java
@@ -11,25 +11,26 @@ public class FeedCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player player) {
-            String playerName;
-            if (args.length == 0) {
-                playerName = "";
+            String targetName;
+            Player target;
+
+            if (args.length != 0) {
+                targetName = args[0];
+                target = Bukkit.getServer().getPlayerExact(targetName);
             } else {
-                playerName = args[0];
+                target = player;
+                targetName = target.getDisplayName();
             }
 
-            Player target = Bukkit.getServer().getPlayerExact(playerName);
-
             if (target == null) {
-                player.sendMessage(ChatColor.RED + "Player with the name " + ChatColor.WHITE + playerName + ChatColor.RED + " was not found");
+                player.sendMessage(ChatColor.RED + "Player with the name " + ChatColor.WHITE + targetName + ChatColor.RED + " was not found");
                 return false;
             }
 
             target.setFoodLevel(20);
-            target.sendMessage(ChatColor.YELLOW + player.getDisplayName() + " filled " + target.getDisplayName() + "'s hunger");
-            if (player.getUniqueId() != target.getUniqueId()) {
-                player.sendMessage(ChatColor.YELLOW + player.getDisplayName() + " filled " + target.getDisplayName() + "'s hunger");
-            }
+
+            target.sendMessage(ChatColor.YELLOW + player.getDisplayName() + " filled your hunger");
+            player.sendMessage(ChatColor.YELLOW + "Filled " + target.getDisplayName() + "'s hunger");
 
             return true;
         }
