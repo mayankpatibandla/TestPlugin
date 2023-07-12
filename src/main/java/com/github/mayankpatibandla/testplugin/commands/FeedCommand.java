0028564b1bad693efdc5f0027a69ef557cb27e10
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-07-11 22:02:28 -0400

    Added target selector support + new messages for feed

--- src/main/java/com/github/mayankpatibandla/testplugin/commands/FeedCommand.java
+++ src/main/java/com/github/mayankpatibandla/testplugin/commands/FeedCommand.java
@@ -5,35 +5,45 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class FeedCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player player) {
-            String targetName;
-            Player target;
+        List<Entity> targets;
 
-            if (args.length != 0) {
-                targetName = args[0];
-                target = Bukkit.getServer().getPlayerExact(targetName);
-            } else {
-                target = player;
-                targetName = target.getDisplayName();
-            }
-
-            if (target == null) {
-                player.sendMessage(ChatColor.RED + "Player with the name " + ChatColor.WHITE + targetName + ChatColor.RED + " was not found");
-                return false;
-            }
-
-            target.setFoodLevel(20);
-
-            target.sendMessage(ChatColor.YELLOW + player.getDisplayName() + " filled your hunger");
-            player.sendMessage(ChatColor.YELLOW + "Filled " + target.getDisplayName() + "'s hunger");
-
+        if (args.length != 0) { // Target Selector
+            targets = Bukkit.selectEntities(sender, args[0]);
+        } else if (sender instanceof Player) { // Player sends command with no argument
+            targets = List.of((Player) sender);
+        } else { // Non-player sends command with no argument
+            sender.sendMessage(ChatColor.RED + "A player must be targeted");
             return true;
         }
-        return false;
+
+        // Player not found
+        if (targets.size() == 0) {
+            sender.sendMessage(ChatColor.RED + "The targeted player(s) could not be found");
+        }
+
+        int n = 0;
+        for (Entity target : targets) {
+            if (target instanceof Player p) {
+                p.setFoodLevel(20);
+                n++;
+
+                p.sendMessage(ChatColor.YELLOW + "Your hunger was filled" + (sender instanceof Player ? " by " + ((Player) sender).getDisplayName() : ""));
+            }
+        }
+
+        if (n == 0) {
+            sender.sendMessage(ChatColor.RED + "No players were found within the selection");
+        } else {
+            sender.sendMessage(ChatColor.GREEN + "Successfully filled the hunger of " + n + " player(s)");
+        }
+        return true;
     }
 }
