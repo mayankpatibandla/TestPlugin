5661f65226f6dc727800d9168c5a1e96ea752777
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-07-14 19:00:43 -0400

    Changed parse coordinate to use Location instead of individual values

--- src/main/java/com/github/mayankpatibandla/testplugin/commands/SetSpawnCommand.java
+++ src/main/java/com/github/mayankpatibandla/testplugin/commands/SetSpawnCommand.java
@@ -2,7 +2,7 @@ package com.github.mayankpatibandla.testplugin.commands;
 
 import com.github.mayankpatibandla.testplugin.TestPlugin;
 import com.github.mayankpatibandla.testplugin.util.Coordinate;
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -18,12 +18,13 @@ public class SetSpawnCommand implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player p) {
-            Location location = new Location(Bukkit.getWorld(p.getUniqueId()),
-                                             Coordinate.parseCoordinate(args[0], p.getLocation().getX()),
-                                             Coordinate.parseCoordinate(args[1], p.getLocation().getY()),
-                                             Coordinate.parseCoordinate(args[2], p.getLocation().getZ()));
+            Location location = Coordinate.parseLocation(args, p.getLocation());
             TestPlugin.getPlugin().getConfig().set("spawn", location);
             TestPlugin.getPlugin().saveConfig();
+
+            sender.sendMessage(ChatColor.GREEN +
+                               String.format("Spawn successfully set to X: %f Y: %f, Z: %f", location.getX(),
+                                             location.getY(), location.getZ()));
         }
         return true;
     }
--- src/main/java/com/github/mayankpatibandla/testplugin/util/Coordinate.java
+++ src/main/java/com/github/mayankpatibandla/testplugin/util/Coordinate.java
@@ -1,22 +1,31 @@
 package com.github.mayankpatibandla.testplugin.util;
 
+import org.bukkit.Location;
+
 import java.text.DecimalFormat;
 
 public class Coordinate {
     public static final DecimalFormat format = new DecimalFormat("#.##");
 
-    public static double parseCoordinate(String arg, double base) {
-        if (arg.startsWith("~")) { // Relative coordinate
-            if (arg.length() > 1) {
-                return Double.parseDouble(arg.substring(1)) + base;
-            } else {
-                return base;
+    public static Location parseLocation(String[] arg, Location original) {
+        double[] base = {original.getX(), original.getY(), original.getZ()};
+        double[] result = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            if (arg[i].startsWith("~")) { // Relative coordinate
+                if (arg[i].length() > 1) {
+                    result[i] = Double.parseDouble(arg[i].substring(1)) + base[i];
+                } else {
+                    result[i] = base[i];
+                }
+            } else if (arg[i].startsWith("^")) { // TODO: Local coordinate
+                System.out.println("Local coordinate WIP");
+                result[i] = Double.parseDouble(arg[i]);
+            } else { // World Coordinate
+                result[i] = Double.parseDouble(arg[i]);
             }
-        } else if (arg.startsWith("^")) { // TODO: Local coordinate
-            System.out.println("Local coordinate WIP");
-            return Double.parseDouble(arg);
-        } else { // World Coordinate
-            return Double.parseDouble(arg);
         }
+
+        return new Location(original.getWorld(), result[0], result[1], result[2]);
     }
 }
